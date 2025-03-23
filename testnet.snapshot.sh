3e07d78f998c4dea91b7a34819a49b646d97cf1f
StakePool <91626069+Michel-Leidson@users.noreply.github.com>
2025-03-23 12:16:21 -0300

    Update testnet.snapshot.sh

--- testnet.snapshot.sh
+++ testnet.snapshot.sh
@@ -13,10 +13,8 @@ fi
 network=$1
 node_type=$2
 
-# URL da lista de snapshots
 snapshot_list_url="https://snap.stakepool.work/snapshots-stakepool/list_snapshots.txt"
 
-# Baixar e filtrar a lista de snapshots para o network e node_type especificados
 relevant_snapshots=$(curl -s "$snapshot_list_url" | grep "$network-$node_type")
 
 if [ -z "$relevant_snapshots" ]; then
@@ -24,7 +22,6 @@ if [ -z "$relevant_snapshots" ]; then
     exit 1
 fi
 
-# Encontrar o snapshot mais recente com base na data e hora
 latest_snapshot=$(echo "$relevant_snapshots" | sort -k1,1r -k2,2r | head -n 1)
 
 if [ -z "$latest_snapshot" ]; then
@@ -32,16 +29,13 @@ if [ -z "$latest_snapshot" ]; then
     exit 1
 fi
 
-# Extrair o nome do arquivo do snapshot mais recente
 snapshot_file=$(echo "$latest_snapshot" | awk '{print $3}')
 
-# Construir a URL completa do snapshot
 url="https://snap.stakepool.work/snapshots-stakepool/$snapshot_file"
 
 echo "Downloading and extracting the latest snapshot for $network - $node_type..."
 echo "Snapshot URL: $url"
 
-# Fazer o download e extrair o snapshot
 wget -c --retry-connrefused --timeout=60 --read-timeout=120 --inet4-only "$url" -O - | dd bs=3G iflag=fullblock | zstdcat | tar -xf -
 
 if [ $? -eq 0 ]; then
